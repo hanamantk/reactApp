c15da963989ceaedbbdba56855a69c8872405407
hanamantk <hanmant2011@hotmail.com>
2018-06-28 10:49:44 +0000

    third cmt

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow, mount} from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('test app compo length',()=>{
+  it('renders 1 app',()=>{
+    const component=shallow(<App />);
+    expect(1).toEqual(1);
+  })
+})
+
+describe('click test',()=>{
+  it('show loadding status when button clicked',()=>{
+    const component=mount(<App />);
+    const button=component.find("DefaultButton");
+          button.simulate('click');
+       expect(component.state().imageLoad).toEqual("Loading...")   
+  })
+})
